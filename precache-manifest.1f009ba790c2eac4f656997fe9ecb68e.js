8eefe76b9956437a15b3ae1408a34b293fdb7c90
Matijahh <matijaradofficial@gmail.com>
2020-10-26 10:42:09 +0100

    Updates

---
+++ precache-manifest.1f009ba790c2eac4f656997fe9ecb68e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f626abf506a8240d352c730916507200",
+    "url": "/nulllion-app/index.html"
+  },
+  {
+    "revision": "5fb9bacd4693872d592b",
+    "url": "/nulllion-app/static/css/2.fb230578.chunk.css"
+  },
+  {
+    "revision": "41f5705fa8bd3a04c600",
+    "url": "/nulllion-app/static/css/main.5548a051.chunk.css"
+  },
+  {
+    "revision": "5fb9bacd4693872d592b",
+    "url": "/nulllion-app/static/js/2.6d71253e.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/nulllion-app/static/js/2.6d71253e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41f5705fa8bd3a04c600",
+    "url": "/nulllion-app/static/js/main.5c0c507f.chunk.js"
+  },
+  {
+    "revision": "0c067979c9524582fcf8",
+    "url": "/nulllion-app/static/js/runtime-main.f8d41d44.js"
+  },
+  {
+    "revision": "a9a110df84e482d1518c55fdc4e58cdf",
+    "url": "/nulllion-app/static/media/10364.a9a110df.jpg"
+  },
+  {
+    "revision": "4898f1f8954bea18c19bde816aacb0ec",
+    "url": "/nulllion-app/static/media/195548-OY9OIN-908.4898f1f8.jpg"
+  },
+  {
+    "revision": "637905880c602930fce335f9a55b3b2f",
+    "url": "/nulllion-app/static/media/25060.63790588.jpg"
+  },
+  {
+    "revision": "b0603a5acd3fdc7efb77a19e0eedb490",
+    "url": "/nulllion-app/static/media/31214.b0603a5a.jpg"
+  },
+  {
+    "revision": "dfc25c7c281cffa354aafa40f2a1045f",
+    "url": "/nulllion-app/static/media/OGN39D0.dfc25c7c.jpg"
+  },
+  {
+    "revision": "b9d67e41322611d48523acca1d28ae80",
+    "url": "/nulllion-app/static/media/contact-us-background.b9d67e41.jpg"
+  },
+  {
+    "revision": "e803563c925d3ff7234b0392ef655522",
+    "url": "/nulllion-app/static/media/health-insurance.e803563c.svg"
+  },
+  {
+    "revision": "6c7021e920f5a7f6b5df9af5553170f3",
+    "url": "/nulllion-app/static/media/investment-plan.6c7021e9.svg"
+  },
+  {
+    "revision": "950babc817a4c00a8655f9d73755494d",
+    "url": "/nulllion-app/static/media/logo.950babc8.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nulllion-app/home/precache-manifest.daa382a2e5f8078e1c909e856034110c.js"
+  "/nulllion-app/precache-manifest.1f009ba790c2eac4f656997fe9ecb68e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nulllion-app/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nulllion-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
